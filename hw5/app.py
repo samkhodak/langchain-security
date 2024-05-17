88a85b00f5548a9d4e53745ed7ba5095c3bcb609
skhodak <khod2@pdx.edu>
2024-05-17 01:22:36 +0000

    added verifying chain for multiple calls.

--- hw5/app.py
+++ hw5/app.py
@@ -5,7 +5,8 @@ from langchain_community.document_loaders.generic import GenericLoader
 from langchain_community.document_loaders.parsers import LanguageParser
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.prompts import PromptTemplate
-from langchain_core.runnables import RunnablePassthrough
+from operator import itemgetter
+from textwrap import dedent
 import traceback
 import time
 import asyncio
@@ -31,7 +32,7 @@ gpt_llm = ChatOpenAI(model="gpt-4o", temperature=0)
 
 
 # Maximum tokens allowed for a prompt, otherwise the chain call will be rejected.
-MAX_PROMPT_TOKENS = 500
+MAX_PROMPT_TOKENS = 1000
 
 
 
@@ -72,7 +73,7 @@ async def run_chain(chain, document):
     Returns:
         str: Result from chain
     """    
-    response = await chain.ainvoke(document)
+    response = await chain.ainvoke({"code_content": document})
     return response
 
 
@@ -100,42 +101,75 @@ async def dual_chains(chain_one, chain_two, document):
 
 def main():
     code_chain = lambda prompt, llm: (
-    {"code_content":RunnablePassthrough()} 
-    | prompt 
+    prompt 
     | llm
     | StrOutputParser()
     )
 
-    prompt = PromptTemplate.from_template("""You are an expert at spotting security vulnerabilities and bad practice in C and C++ code.
-        Your job is to take in a segment of code and identify the top three vulnerabilities that can be found in it. 
+    final_chain = lambda prompt, verifying_prompt, llm: (
+    {"code_content": itemgetter("code_content"), "previous_results":code_chain(prompt, llm)} 
+    | code_chain(verifying_prompt, llm)
+    )
+
+
+    generative_prompt = PromptTemplate.from_template(dedent("""You are an expert at spotting security vulnerabilities and bad practice in C and C++ code.
+        Your job is to take in a segment of code and identify the top three vulnerabilities that can be found in it.
         Keep your answer concise, but list out the top three results with enough detail that the reader can understand how to solve the security issue.
+        If you can't find three important vulnerabilities, do not make any up. Just list as many as you can, up to three.
         You are allowed to reference functions and one-line bits of code to help the reader, but do not include code blocks in your answer. Print all code lines on a new line.
         Your answer should be in the following format:
-        Here are three of the top security vulnerabilities in the provided [language] code.
+        Here are [number, 3 or less] of the top security vulnerabilities in the provided [language] code.
         1. [vulnerability 1]
            - Issue: [explanation of vulnerability]
            - Recommendation: [explanation of solution]
 
         ... and so on.
-        Code content: {code_content}
-    """)
 
-    gemini_chain = code_chain(prompt, gemini_llm)
-    gpt_chain = code_chain(prompt, gpt_llm)
+        Code content: 
+        {code_content}
+    """))
+
+    verifying_prompt= PromptTemplate.from_template(dedent("""You are an expert at veryifying security vulnerabilities in C and C++ code.
+        Your job is to take in a segment of code and a list of potential vulnerabilities in it, and check if the reasoning for the issues listed is accurate. 
+        If the reasoning is incorrect or there are more important/dangerous vulnerabilities in the code provided, replace any number of the original vulnerabilities
+        with an updated version of the vulnerability or a more important vulnerability.
+        Keep your answer concise, but list out the top three results with enough detail that the reader can understand how to solve the security issue.
+        If you can't find three important vulnerabilities, do not make any up. Just list as many as you can, up to three.
+        You are allowed to reference functions and one-line bits of code to help the reader, but do not include code blocks in your answer. Print all code lines on a new line.
+        Your answer should be in the following format:
+        Here are [number, 3 or less] of the top security vulnerabilities in the provided [language] code.
+        1. [vulnerability 1]
+           - Issue: [explanation of vulnerability]
+           - Recommendation: [explanation of solution]
+
+        ... and so on.
+
+        Code content: 
+        {code_content}
+        List of vulnerabilities to verify: 
+        {previous_results}
+    """))
+
+    gemini_chain = final_chain(generative_prompt, verifying_prompt, gemini_llm)
+    gpt_chain = final_chain(generative_prompt, verifying_prompt, gpt_llm)
 
 
+    print("Welcome to C/C++ Vulnerability Identifier. Store a C/C++ file in the sources folder and enter its name to have its vulnerabilities checked!")
 
     while True:
         try:
             line = input("\n\nEnter query (\"exit\" to end) >>  ")
             if line and line != "exit": 
                 document = load_code(line)
-                if (gemini_llm.get_num_tokens(document) > MAX_PROMPT_TOKENS):
-                    raise RuntimeError(f"The document you are trying to check is too large for the {MAX_PROMPT_TOKENS} token limit.")
+                document_token_count = gemini_llm.get_num_tokens(document)
+                if (document_token_count > MAX_PROMPT_TOKENS):
+                    raise RuntimeError(f"The document you are trying to enter ({document_token_count} tokens) is too large for the {MAX_PROMPT_TOKENS} token limit.")
                 
                 start = time.perf_counter()
-                asyncio.run(dual_chains(gemini_chain, gpt_chain, document))
+                gemini_result, gpt_result = asyncio.run(dual_chains(gemini_chain, gpt_chain, document))
                 time_taken = time.perf_counter() - start
+                print("\nGemini's Result: \n", gemini_result)
+                print("\nGPT-4o's Result: \n", gpt_result)
                 print(f"""\n\nTime taken to complete both requests: {"{:.2f}".format(time_taken)} seconds.""")
 
             else:
