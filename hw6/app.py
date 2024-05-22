dddea3fb1f15f172ef9cafedfcd6db26bd73be69
skhodak <khod2@pdx.edu>
2024-05-22 00:54:39 +0000

    added ip_location_info tool and updated documentation.

--- hw6/app.py
+++ hw6/app.py
@@ -4,8 +4,7 @@ from langsmith import Client
 from langchain import hub
 from langchain_core.pydantic_v1 import BaseModel, Field, validator
 from langchain.tools import tool
-from langchain_core.runnables import RunnablePassthrough
-import dns.resolver, dns.reversename
+import requests
 import socket
 import validators
 import traceback
@@ -28,34 +27,71 @@ gemini_llm = GoogleGenerativeAI(
     }
 )
 
-class LookupIPInput(BaseModel):
-    address: str = Field(description="Should be an IP address such as 208.91.197.27 or 143.95.239.83")
+class IPv4Input(BaseModel):
+    """ 
+    For checking correctness of IPv4 address passed to tools.
+
+    :param address: IPv4 address, without CIDR notation.
+    :type address: str
+    """
+    address: str = Field(description="Should be an IP address such as 208.91.197.27 or 143.95.239.83, with no CIDR notation.")
     @validator('address')
     def is_ip_address(cls, value: str) -> str:
         if not validators.ip_address.ipv4(value):
             raise ValueError("Malformed IP address")
         return value
 
-@tool("retrieve_DNS_name", args_schema=LookupIPInput, return_direct=False)
+
+@tool("retrieve_DNS_name", args_schema=IPv4Input, return_direct=False)
 def retrieve_DNS_name(ip_address):
     """
     Given an IPv4 address, returns DNS hostname associated with it
+
+    :param ip_address: IPv4 address, without CIDR notation.
+    :type ip_address: str
+    :raises ValueError: Hostname is rejected if it's not able to be found.
+    :return: DNS hostname for IP passed in
+    :rtype: str
     """
     try:
-        hostname, _, _ = socket.gethostbyaddr(address)
+        hostname, _, _ = socket.gethostbyaddr(ip_address)
         return hostname
     except socket.herror:
-        raise ValueError("The hostname is not valid. Please enter an IPv4 address.")
+        raise ValueError("The hostname is not valid. Please enter an IPv4 address with no CIDR notation.")
+
+
+@tool("ip_location_info", args_schema=IPv4Input, return_direct=False)
+def ip_location_info(ip_address):
+    """
+    Get relevant location and organization information for an IP address.
+
+    :param ip_address: IPv4 address, without CIDR notation.
+    :type ip_address: str
+    :return: dictionary of location info, including organization
+    :rtype: dictionary of strings
+    """
+    response = requests.get(f'https://ipapi.co/{ip_address}/json/').json()
+    location_info = {
+        'location': f"{response.get('city','N/A')}, {response.get('region','N/A')} - {response.get('country_name','N/A')} ({response.get('continent_code','N/A')})",
+        'latitude': response.get('latitude', 'N/A'),
+        'longitude': response.get('longitude', 'N/A'),
+        'organization': response.get('org', 'N/A')
+    }
+    return location_info
+    
+
+
 
 
 def main():
     base_prompt = hub.pull("langchain-ai/react-agent-template")
     prompt = base_prompt.partial(instructions="""You are an agent that is used for helping the user get information about IP addresses.
         Be as helpful as possible. If you are unable to produce an answer that is helpful to the user, say so.
-        The user is allowed to look up information related to IP addresses ONLY. Deny them in any other case.""")
+        The user is allowed to look up information related to IP addresses ONLY. Deny them in any other case.
+        Because your tools provide a lot of dense information, structure your response by separating each tool call's answer in a visually pleasing list.""")
 
     tools = load_tools(["serpapi"])
-    tools.extend([lookup_ip])
+    tools.extend([retrieve_DNS_name, ip_location_info])
 
     gemini_agent = create_react_agent(gemini_llm, tools, prompt)
     gemini_executor = AgentExecutor(
@@ -68,6 +104,7 @@ def main():
 
     for tool in gemini_executor.tools:
         print(f"\n{tool.name}: \n\n{tool.description}")
+    
 
     while True:
         try:
